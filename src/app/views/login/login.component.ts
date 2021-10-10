6184f9001689083750b409aa16d442e887baef74
Hamza Benali <b.a.hamza1991@gmail.com>
2021-10-10 12:29:03 +0100

    login completed

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,7 @@
 
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToasterService } from 'angular2-toaster';
 import { HttpService } from '../../services/http.service';
 
@@ -11,22 +13,48 @@ export class LoginComponent implements OnInit {
 
   constructor(private toasterService: ToasterService, private http: HttpService) { }
   users: any
+  loginForm: FormGroup
+  status = false
   ngOnInit(): void {
     this.showSuccess();
 
-    this.http.getAllUsers().subscribe(res => {
-      this.users = res
-    }, err => {
-      console.log(err);
+    this.loginForm = new FormGroup({
+      "email": new FormControl(null, [Validators.email, Validators.required]),
+      "password": new FormControl(null, Validators.required)
 
-    })
+    });
+    this.loginForm.valueChanges.subscribe( // this is for updating password input to compare it to password 2 on the confirmpassword validator 
+      (value: any) => {
+
+        this.status = this.loginForm.status === 'VALID' ? true : false // this is for updating disablied button 
+        console.log(this.status);
+
+      }
+    );
+
   }
 
   showSuccess() {
     this.toasterService.pop('Primary', 'Ink-Aroma', 'Welcomes you back .');
   }
   onclick() {
-    alert(JSON.stringify(this.users))
+  }
+  OnSubmit() {
+    this.http.login(this.loginForm.value).subscribe(res => {
+      console.log(res);
+      this.toasterService.pop("success", "Ink-Aroma", "Login succeded")
+    }, err => {
+      console.log(err);
+      if (err.error.message === 'Please make sure the email and password are correct .') {
+        this.toasterService.pop("warning", "login failed", "'Please make sure the email and password are correct .'");
+        
+      } else {
+        console.log(err.error);
+
+      }
+
+    })
+
   }
 
 
